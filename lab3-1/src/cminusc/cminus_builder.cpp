4a41db93e7e99aa6617c34b53eb4e17dd43aa329
ChengYang <Chernyang@163.com>
2019-11-25 20:25:18 +0800

    debug

--- lab3-1/src/cminusc/cminus_builder.cpp
+++ lab3-1/src/cminusc/cminus_builder.cpp
@@ -2,42 +2,37 @@
 #include <iostream>
 using namespace llvm;
 using namespace std;
+
 #define CONST(num) \
     ConstantInt::get(context, APInt(32, num)) //得到常数值的表示,方便后面多次用到
+#define TyInt32 Type::getInt32Ty(context)
+#define DEBUG true
 
 Function *func;
 Value *Exp_val;
-Value *Bool_val;
-// int exp_val;
-//Value *Var_addr;
-// int var_addr;
 int stmt_num;
 
-#define TyInt32 llvm::Type::getInt32Ty(context)
-
-// You can define global variables here
-// to store state
 
 void CminusBuilder::visit(syntax_program &node)
 {
-    // printf("program begin:\n");
+    if (DEBUG) printf("program begin:\n");
     for (auto decl : node.declarations)
     {
         decl->accept(*this);
     }
-    // printf("program end:\n");
+    if (DEBUG) printf("program end:\n");
 }
 
 void CminusBuilder::visit(syntax_num &node)
 {
-    // printf("num begin:\n");
+    if (DEBUG) printf("num begin:\n");
     Exp_val = CONST(node.value);
-    // printf("num end:\n");
+    if (DEBUG) printf("num end:\n");
 }
 
 void CminusBuilder::visit(syntax_var_declaration &node)
 {
-    // printf("var_declaration begin:\n");
+    if (DEBUG) printf("var_declaration begin:\n");
     int temp = 0;
     Type *TYPE32 = Type::getInt32Ty(context);
 
@@ -73,14 +68,13 @@ void CminusBuilder::visit(syntax_var_declaration &node)
             scope.push(node.id, local);
         }
     }
-    // printf("var_declaration end:\n");
+    if (DEBUG) printf("var_declaration end:\n");
 }
 
 void CminusBuilder::visit(syntax_fun_declaration &node)
 {
-    // printf("fun_declaration begin:\n");
+    if (DEBUG) printf("fun_declaration begin:\n");
     auto TyVoid = llvm::Type::getVoidTy(context);
-    // std::vector<Type *> Ints(node.params.size(), TyInt32);
     vector<Type *> Ints;
     std::vector<Value *> args2; //获取gcd函数的参数,通过iterator
     for (auto i : node.params)
@@ -136,18 +130,17 @@ void CminusBuilder::visit(syntax_fun_declaration &node)
     for (auto i : node.params)
     {
         auto id1 = scope.find(i->id);
-        // id1 = builder.CreateGEP(id1, {CONST(0),CONST(0)});
         builder.CreateStore(args[index], id1);
         index++;
     }
     node.compound_stmt->accept(*this);
     scope.exit();
-    // printf("fun_declaration end:\n");
+    if (DEBUG) printf("fun_declaration end:\n");
 }
 
 void CminusBuilder::visit(syntax_param &node)
 {
-    // printf("param begin:\n");
+    if (DEBUG) printf("param begin:\n");
     if (node.type == TYPE_INT)
     {
         if (node.isarray)
@@ -161,12 +154,12 @@ void CminusBuilder::visit(syntax_param &node)
             scope.push(node.id, uAlloca);
         }
     }
-    // printf("param end:\n");
+    if (DEBUG) printf("param end:\n");
 }
 
 void CminusBuilder::visit(syntax_compound_stmt &node)
 {
-    // printf("compound_stmt begin:\n");
+    if (DEBUG) printf("compound_stmt begin:\n");
     scope.enter();
     for (auto delc : node.local_declarations)
     {
@@ -179,25 +172,23 @@ void CminusBuilder::visit(syntax_compound_stmt &node)
     for (auto stmt : node.statement_list)
     {
         stmt_num--;
-        // printf("syntax_compound_stmt stmt_num: %d\n", stmt_num);
         stmt->accept(*this);
     }
     scope.exit();
-    // printf("compound_stmt end:\n");
+    if (DEBUG) printf("compound_stmt end:\n");
 }
 
 void CminusBuilder::visit(syntax_expresion_stmt &node)
 {
-    // printf("expresion_stmt begin:\n");
+    if (DEBUG) printf("expresion_stmt begin:\n");
     if (node.expression != nullptr)
         node.expression->accept(*this);
-    // printf("expresion_stmt end:\n");
+    if (DEBUG) printf("expresion_stmt end:\n");
 }
 
 void CminusBuilder::visit(syntax_selection_stmt &node)
 {
-    // std::cout<<"selection_stmt begin"<<endl;
-    // printf("selection_stmt begin:\n");
+    if (DEBUG) printf("selection_stmt begin:\n");
     if (stmt_num > 0)
     {
         if (node.else_statement != nullptr)
@@ -225,7 +216,6 @@ void CminusBuilder::visit(syntax_selection_stmt &node)
                 builder.CreateBr(endBB);
             }
 
-            // builder.CreateBr(endBB);
             builder.SetInsertPoint(endBB);
         }
         else
@@ -235,7 +225,6 @@ void CminusBuilder::visit(syntax_selection_stmt &node)
 
             node.expression->accept(*this);
             auto icmp = builder.CreateICmpNE(Exp_val, CONST(0));
-            // printf("select begin:\n");
             auto br = builder.CreateCondBr(icmp, trueBB, endBB);
 
             builder.SetInsertPoint(trueBB);
@@ -262,13 +251,12 @@ void CminusBuilder::visit(syntax_selection_stmt &node)
             node.else_statement->accept(*this);
         }
     }
-    // std::cout<<"selection_stmt end"<<endl;
-    // printf("selection_stmt end:\n");
+    if (DEBUG) printf("selection_stmt end:\n");
 }
 
 void CminusBuilder::visit(syntax_iteration_stmt &node)
 {
-    // printf("iteration_stmt beign:\n");
+    if (DEBUG) printf("iteration_stmt beign:\n");
     auto jugBB = llvm::BasicBlock::Create(context, "jugBB", func);
     auto l_trueBB = llvm::BasicBlock::Create(context, "trueBB", func);
     auto l_falseBB = llvm::BasicBlock::Create(context, "falseBB", func);
@@ -283,16 +271,13 @@ void CminusBuilder::visit(syntax_iteration_stmt &node)
     node.statement->accept(*this);
     builder.CreateBr(jugBB);
 
-    // printf("stmt_num: %d\n", stmt_num);
     builder.SetInsertPoint(l_falseBB);
-    // std::cout<<"iteration_stmt end"<<endl;
-    // printf("iteration_stmt end:\n");
+    if (DEBUG) printf("iteration_stmt end:\n");
 }
 
 void CminusBuilder::visit(syntax_return_stmt &node)
 {
-    // std::cout<<"return_stmt begin"<<endl;
-    // printf("return_stmt begin:\n");
+    if (DEBUG) printf("return_stmt begin:\n");
     if (node.expression == nullptr)
     {
         builder.CreateRetVoid();
@@ -303,22 +288,18 @@ void CminusBuilder::visit(syntax_return_stmt &node)
         node.expression->accept(*this); //得到expression的值
         builder.CreateRet(Exp_val);     //全局变量 表达式的值
     }
-    // std::cout<<"return_stmt end"<<endl;
-    // printf("return_stmt end:\n");
+    if (DEBUG) printf("return_stmt end:\n");
 }
 
 void CminusBuilder::visit(syntax_var &node)
 {
     // 取出变量的值
-    // std::cout<<"var begin"<<endl;
+    if (DEBUG) std::cout<<"var begin"<<endl;
     llvm::Value *Var_addr = scope.find(node.id);
-    //auto check_end = BasicBlock::Create(context, "check_end", func);
     if (node.expression != nullptr)
     {
         node.expression->accept(*this);
-        //Var_addr = builder.CreateGEP(Var_addr, {CONST(0),Exp_val}, node.id);
         auto icmp = builder.CreateICmpSGE(Exp_val, CONST(0));
-        //auto check_end = BasicBlock::Create(context, "check_end", func);
         auto except = BasicBlock::Create(context, "except", func);
         auto normal = BasicBlock::Create(context, "normal", func);
 
@@ -351,19 +332,17 @@ void CminusBuilder::visit(syntax_var &node)
         Exp_val = builder.CreateLoad(Var_addr);
     }
 
-    // std::cout<<"var end"<<endl;
+    if (DEBUG) std::cout<<"var end"<<endl;
 }
 
 void CminusBuilder::visit(syntax_assign_expression &node)
 {
-    // printf("assign_expression begin:\n");
-    // std::cout<<"assign_expression begin"<<endl;
+    if (DEBUG) printf("assign_expression begin:\n");
     llvm::Value *Var_addr = scope.find(node.var->id);
     if (node.var->expression != nullptr)
     {
         node.var->expression->accept(*this);
         auto icmp = builder.CreateICmpSGE(Exp_val, CONST(0));
-        //auto check_end = BasicBlock::Create(context, "check_end", func);
         auto except = BasicBlock::Create(context, "except", func);
         auto normal = BasicBlock::Create(context, "normal", func);
 
@@ -386,19 +365,16 @@ void CminusBuilder::visit(syntax_assign_expression &node)
         }
     }
 
-    //node.var->accept(*this);
     //调用 var 得到地址后，直接使用。
     node.expression->accept(*this);
 
     builder.CreateStore(Exp_val, Var_addr);
-    // printf("assign_expression end:\n");
-    // std::cout<<"assign_expression end"<<endl;
+    if (DEBUG) printf("assign_expression end:\n");
 }
 
 void CminusBuilder::visit(syntax_simple_expression &node)
 {
-    // std::cout<<"simple_expression begin"<<endl;
-    // printf("simple_expression begin:\n");
+    if (DEBUG) printf("simple_expression begin:\n");
     if (node.additive_expression_r == nullptr)
     {
         node.additive_expression_l->accept(*this);
@@ -431,10 +407,8 @@ void CminusBuilder::visit(syntax_simple_expression &node)
         }
         else if (node.op == OP_EQ)
         {
-            // printf("EQ begin:\n");
             Exp_val = builder.CreateICmpEQ(addiexpr1, addiexpr2);
             Exp_val = builder.CreateIntCast(Exp_val, Type::getInt32Ty(context), false);
-            // printf("EQ end:\n");
         }
         else if (node.op == OP_NEQ)
         {
@@ -442,14 +416,12 @@ void CminusBuilder::visit(syntax_simple_expression &node)
             Exp_val = builder.CreateIntCast(Exp_val, Type::getInt32Ty(context), false);
         }
     }
-    // std::cout<<"simple_expression end"<<endl;
-    // printf("simple_expression end:\n");
+    if (DEBUG) printf("simple_expression end:\n");
 }
 
 void CminusBuilder::visit(syntax_additive_expression &node)
 {
-    // std::cout<<"additive_expression begin"<<endl;
-    // printf("additive_expression begin:\n");
+    if (DEBUG) printf("additive_expression begin:\n");
     if (node.additive_expression == nullptr)
     {
         node.term->accept(*this);
@@ -469,14 +441,12 @@ void CminusBuilder::visit(syntax_additive_expression &node)
             Exp_val = builder.CreateNSWSub(addiexpr, term);
         }
     }
-    // printf("additive_expression end:\n");
-    // std::cout<<"additive_expression end"<<endl;
+    if (DEBUG) printf("additive_expression end:\n");
 }
 
 void CminusBuilder::visit(syntax_term &node)
 {
-    // std::cout<<"term begin"<<endl;
-    // printf("term begin:\n");
+    if (DEBUG) printf("term begin:\n");
     if (node.term == nullptr)
     {
         node.factor->accept(*this);
@@ -496,14 +466,12 @@ void CminusBuilder::visit(syntax_term &node)
             Exp_val = builder.CreateSDiv(term, factor);
         }
     }
-    // printf("term end:\n");
-    // std::cout<<"term end"<<endl;
+    if (DEBUG) printf("term end:\n");
 }
 
 void CminusBuilder::visit(syntax_call &node)
 {
-    // std::cout<<"call begin"<<endl;
-    // printf("call begin:\n");
+    if (DEBUG) printf("call begin:\n");
     auto CalleeF = scope.find(node.id);
     vector<Value *> Argu;
     for (auto s = node.args.begin(); s != node.args.end(); s++)
@@ -512,10 +480,12 @@ void CminusBuilder::visit(syntax_call &node)
         // 如果是数组类型，通过这条语句将指向数组的指针转换成指向数组首元素的指针，
         // 第一个CONST(0)意思是元素的首元素从零开始，第二个CONST(0)表示首元素。
         if (Exp_val->getType()->isPointerTy())
-            Exp_val = builder.CreateGEP(Exp_val, {CONST(0), CONST(0)});
+        {
+            if (Exp_val->getType()->getPointerElementType()->isArrayTy())
+                Exp_val = builder.CreateGEP(Exp_val, {CONST(0), CONST(0)});
+        }
         Argu.push_back(Exp_val);
     }
     Exp_val = builder.CreateCall(CalleeF, Argu);
-    // printf("call end:\n");
-    // std::cout<<"call end"<<endl;
+    if (DEBUG) printf("call end:\n");
 }
